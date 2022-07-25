00bae08afdd61bd9ccd52787e15572965c9accda
Philipp04 <58029919+Philipp04@users.noreply.github.com>
2022-07-25 19:02:06 +0200

    login page start

--- lib/main.dart
+++ lib/main.dart
@@ -11,71 +11,75 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Introduction(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class Introduction extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _IntroductionState createState() => _IntroductionState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _IntroductionState extends State<Introduction> {
   final controller = PageController(viewportFraction: 0.8, keepPage: true);
   static List<String> images = [
     'assets/earth_loadingscreen.png',
-    'assets/desktopsitter_loadingscreen.png'
+    'assets/desktopsitter_loadingscreen.png',
+    'assets/standingunderwear_loadingscreen.png'
   ];
-  static const List<String> texts = [
+  static const List<String> titles = [
     'Connect with People',
+    'Your way!',
+    'Find Your Duo,',
+  ];
+
+  static const List<String> texts = [
     'Find friends anytime, any where, any situation!',
     'Make some friends with same interests',
-    'Your way!'
+    'Make connections that are more meaningful'
   ];
 
   Widget build(BuildContext context) {
     List pages = List.generate(
-        2,
-        (index) =>
-           Stack(
+        3,
+        (index) => Stack(
               alignment: Alignment.center,
               children: [
                 Positioned(
-                  top: 50,
+                  top: MediaQuery.of(context).size.height * 0.22,
                   child: Container(
                     margin: EdgeInsets.symmetric(horizontal: 0, vertical: 4),
-                    height: 375,
-                    width: 275,
+                    height: MediaQuery.of(context).size.height * 0.3587041,
+                    width: MediaQuery.of(context).size.width * 0.5840,
                     decoration: BoxDecoration(
                       image: DecorationImage(
-                        fit: BoxFit.fitWidth,
                         image: AssetImage(images[index]),
                       ),
                     ),
                   ),
                 ),
                 Positioned(
-                  top: 400,
+                  top: MediaQuery.of(context).size.height * 0.6106,
                   child: Container(
-                    height: 31,
-                    width: 250,
-                    child: Text(texts[index],
+                    height: MediaQuery.of(context).size.height * 0.0739,
+                    width: MediaQuery.of(context).size.width * 0.6233,
+                    child: Text(titles[index],
                         textAlign: TextAlign.center,
                         style: TextStyle(
                             fontFamily: 'Akshar',
-                            fontSize: 30,
+                            fontSize: 28,
                             fontWeight: FontWeight.w400,
                             letterSpacing: 0.36,
                             color: Colors.white)),
                   ),
                 ),
                 Positioned(
-                  top: 440,
+                  top: MediaQuery.of(context).size.height * 0.667,
                   child: Container(
-                    height: 100,
+                    height: MediaQuery.of(context).size.height * 0.1463,
                     width: 200,
-                    child: Text(texts[index + 1],
+                    child: Text(texts[index],
                         textAlign: TextAlign.center,
                         style: TextStyle(
                             fontFamily: 'AbhayaLibre-SemiBold',
@@ -98,39 +102,61 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Container(),
-                Container(
-                  height: 520,
+          child: PageView(
+            children: [
+              Column(
+                children: <Widget>[
+                  Container(),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.7519,
                     child: PageView.builder(
-                    controller: controller,
-                    // itemCount: pages.length,
-                    itemBuilder: (_, index) {
-                      return pages[index % pages.length];
-                    },
+                      controller: controller,
+                      // itemCount: pages.length,
+                      itemBuilder: (_, index) {
+                        return pages[index % pages.length];
+                      },
+                    ),
                   ),
-                ),
-                Positioned(
-                  top: 400,
-                  child:
-                SmoothPageIndicator(
-                  controller: controller,
-                  count: pages.length,
-                  effect: const WormEffect(
-                    dotHeight: 16,
-                    dotWidth: 16,
-                    type: WormType.thin,
-                    // strokeWidth: 5,
+                  Positioned(
+                    top: MediaQuery.of(context).size.height * 0.5853,
+                    child: SmoothPageIndicator(
+                      controller: controller,
+                      count: pages.length,
+                      effect: const WormEffect(
+                        dotHeight: 16,
+                        dotWidth: 16,
+                        type: WormType.thin,
+                        // strokeWidth: 5,
+                      ),
+                    ),
                   ),
-                ),
-                ),
-              ],
-            ),
+                  Positioned(
+                    top:  MediaQuery.of(context).size.width * 0.8,
+                    child: ElevatedButton(
+                      onPressed: () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      ),
+                      child: Text('Left to Right'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
